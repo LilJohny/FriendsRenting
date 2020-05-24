3a85205b288a2b29ba1bfcf241059b8507a97f8b
Dan <klymenko@ucu.edu.ua>
2020-05-24 21:18:46 +0300

    implemented present and client classes

---
+++ src/models/client.py
@@ -0,0 +1,10 @@
+from app import db
+
+
+class Client(db.Model):
+    client_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=False, nullable=False)
+    surname = db.Column(db.String(30), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Client %r>' % ''.join([self.name, self.surname])
