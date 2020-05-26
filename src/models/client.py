c31f1370b876f3bbef969c8420dd133942e33900
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-05-27 02:08:56 +0300

    Better length limits for client columns

--- src/models/client.py
+++ src/models/client.py
@@ -5,10 +5,10 @@ class Client(db.Model):
     client_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=False, nullable=False)
     surname = db.Column(db.String(30), unique=False, nullable=False)
-    mail = db.Column(db.String(25), unique=True, nullable=False)
+    mail = db.Column(db.String(40), unique=True, nullable=False)
     sex = db.Column(db.String(1), unique=False, nullable=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
-    address = db.Column(db.String(50), unique=False, nullable=True)
+    address = db.Column(db.String(200), unique=False, nullable=True)
     birth_date = db.Column(db.Date, unique=False, nullable=True)
 
     def __repr__(self):
