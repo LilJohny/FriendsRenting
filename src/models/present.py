0cc901123482f512aad44f23d367807eee21226a
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-05-27 01:48:45 +0300

    Fixed present model

--- src/models/present.py
+++ src/models/present.py
@@ -4,8 +4,8 @@ from models import db
 class Present(db.Model):
     present_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(30), unique=False, nullable=False)
-    _from = db.Column(db.Integer, db.ForeignKey('friend.friend_id', ondelete='CASCADE'), unique=False, nullable=False)
-    to = db.Column(db.Integer, db.ForeignKey('client.client_id', ondelete='CASCADE'), unique=False, nullable=False)
+    _from = db.Column(db.Integer, db.ForeignKey('client.client_id', ondelete='CASCADE'), unique=False, nullable=False)
+    to = db.Column(db.Integer, db.ForeignKey('friend.friend_id', ondelete='CASCADE'), unique=False, nullable=False)
     returned = db.Column(db.Boolean, default=False, unique=False, nullable=False)
 
     def __repr__(self):
