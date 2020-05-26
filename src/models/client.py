c38cf031ae65733f936254334d9914702cc24a7b
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-05-26 23:47:56 +0300

    Added mail to friend and client

--- src/models/client.py
+++ src/models/client.py
@@ -5,6 +5,7 @@ class Client(db.Model):
     client_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=False, nullable=False)
     surname = db.Column(db.String(30), unique=False, nullable=False)
+    mail = db.Column(db.String(25), unique=True, nullable=False)
 
     def __repr__(self):
         return '<Client %r>' % ''.join([self.name, self.surname])
--- src/models/friend.py
+++ src/models/friend.py
@@ -5,6 +5,7 @@ class Friend(db.Model):
     friend_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=False, nullable=False)
     surname = db.Column(db.String(30), unique=False, nullable=False)
+    mail = db.Column(db.String(25), unique=True, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % ''.join([self.name, self.surname])
