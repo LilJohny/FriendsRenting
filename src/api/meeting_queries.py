d74695586f858accbc242b12884abf2ef282689e
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-05-27 05:22:04 +0300

    Implemented get_meetings_number_by_months

--- src/api/meeting_queries.py
+++ src/api/meeting_queries.py
@@ -1,5 +1,6 @@
 import json
 
+import sqlalchemy
 from flask import request
 from flask_restful import Resource
 from sqlalchemy.orm import Session
@@ -22,10 +23,11 @@ class MeetingQueries(Resource):
             name = request.form['name']
             surname = request.form['surname']
             return MeetingQueries.get_meetings_by_friend_name(session, name, surname)
-
         elif request_type == 'get_meetings_by_friend_id':
             friend_id = request.form['friend_id']
             return MeetingQueries.get_meetings_by_friend_id(session, friend_id)
+        elif request_type == 'get_meetings_number_by_months':
+            return MeetingQueries.get_meetings_number_by_months(session)
 
     @staticmethod
     def get_all(session):
@@ -35,7 +37,7 @@ class MeetingQueries(Resource):
 
     @staticmethod
     def get_meetings_by_friend_name(session, name, surname):
-        meeting_by_friend_name = session.query(Meeting).join(FriendGroup).\
+        meeting_by_friend_name = session.query(Meeting).join(FriendGroup). \
             join(FriendGroupRecord).join(Friend).filter(Friend.name == name).filter(Friend.surname == surname).all()
         response = json.dumps(meeting_by_friend_name, cls=AlchemyEncoder)
         return response
@@ -45,4 +47,11 @@ class MeetingQueries(Resource):
         meeting_by_friend_id = session.query(Meeting).join(FriendGroup). \
             join(FriendGroupRecord).join(Friend).filter(Friend.friend_id == friend_id).all()
         response = json.dumps(meeting_by_friend_id, cls=AlchemyEncoder)
+        return response
+
+    @staticmethod
+    def get_meetings_number_by_months(session):
+        month = sqlalchemy.func.date_trunc('month', Meeting.date)
+        result = session.query(sqlalchemy.func.count(Meeting.meeting_id),month ).group_by(month).all()
+        response = json.dumps(result, cls=AlchemyEncoder)
         return response
