acc8d13027195585d548f3169e731b84b7a59374
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-06-02 17:42:51 +0300

    Added Profile modelC

--- src/models/client.py
+++ src/models/client.py
@@ -3,13 +3,7 @@ from models import db
 
 class Client(db.Model):
     client_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), unique=False, nullable=False)
-    surname = db.Column(db.String(30), unique=False, nullable=False)
-    mail = db.Column(db.String(40), unique=True, nullable=False)
-    sex = db.Column(db.String(1), unique=False, nullable=True)
-    username = db.Column(db.String(30), unique=True, nullable=False)
-    address = db.Column(db.String(200), unique=False, nullable=True)
-    birth_date = db.Column(db.Date, unique=False, nullable=True)
+    profile_id = db.Column(db.Integer, db.ForeignKey('profile.profile_id'), unique=True, nullable=False)
 
     def __repr__(self):
-        return '<Client %r>' % ' '.join([self.name, self.surname])
+        return '<Client>'
---
+++ src/models/profile.py
@@ -0,0 +1,16 @@
+from models import db
+
+
+class Profile(db.Model):
+    profile_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=False, nullable=False)
+    surname = db.Column(db.String(30), unique=False, nullable=False)
+    mail = db.Column(db.String(40), unique=True, nullable=False)
+    sex = db.Column(db.String(1), unique=False, nullable=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    address = db.Column(db.String(200), unique=False, nullable=True)
+    birth_date = db.Column(db.Date, unique=False, nullable=True)
+    password = db.Column(db.String(30), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Profile %r>' % ' '.join([self.name, self.surname])
