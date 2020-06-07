2b4dd9b290da08255598a1b15f086a63cad60c1b
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-06-07 17:03:58 +0300

    A lot of work done

--- src/api/client_queries.py
+++ src/api/client_queries.py
@@ -2,6 +2,7 @@ import json
 
 from flask import request
 from flask_restful import Resource
+from sqlalchemy import text
 from sqlalchemy.orm import Session
 
 from models import engine
@@ -27,7 +28,7 @@ class ClientQueries(Resource):
             surname = request.form['surname']
             return ClientQueries.get_compliant_to_friend(session, name, surname)
 
-        elif request_type ==  'get_compliant_to_friend_id':
+        elif request_type == 'get_compliant_to_friend_id':
             friend_id = request.form['friend_id']
             return ClientQueries.get_compliant_to_friend_id(session, friend_id)
 
@@ -40,6 +41,14 @@ class ClientQueries(Resource):
             surname = request.form['surname']
             return ClientQueries.get_hired_friend(session, name, surname)
 
+        elif request_type == 'get_clients_who_rented_by_date_rents_and_number':
+            friend_id = request.form['friend_id']
+            start_date = request.form['start_date']
+            end_date = request.form['end_date']
+            rents = request.form['rents']
+            return ClientQueries.get_clients_who_rented_by_date_rents_and_number(engine, friend_id, start_date,
+                                                                                 end_date, rents)
+
     @staticmethod
     def get_all(session):
         clients_all = session.query(Client).all()
@@ -48,7 +57,7 @@ class ClientQueries(Resource):
 
     @staticmethod
     def get_compliant_to_friend(session, name, surname):
-        clients_compliant_to_friend = session.query(Client).join(ClientGroupRecord).join(ClientGroup).\
+        clients_compliant_to_friend = session.query(Client).join(ClientGroupRecord).join(ClientGroup). \
             join(Complaint).join(Friend).filter(Friend.name == name).filter(Friend.surname == surname).all()
         response = json.dumps(clients_compliant_to_friend, cls=AlchemyEncoder)
         return response
@@ -62,31 +71,30 @@ class ClientQueries(Resource):
 
     @staticmethod
     def get_hired_friend_id(session, friend_id):
-        clients_hired_friend = session.query(Client).join(Meeting).join(FriendGroup).join(FriendGroupRecord).\
+        clients_hired_friend = session.query(Client).join(Meeting).join(FriendGroup).join(FriendGroupRecord). \
             join(Friend).filter(Friend.friend_id == friend_id).all()
         response = json.dumps(clients_hired_friend, cls=AlchemyEncoder)
         return response
 
     @staticmethod
     def get_hired_friend(session, name, surname):
-        clients_hired_friend = session.query(Client).join(Meeting).join(FriendGroup).join(FriendGroupRecord).\
+        clients_hired_friend = session.query(Client).join(Meeting).join(FriendGroup).join(FriendGroupRecord). \
             join(Friend).filter(Friend.name == name).filter(Friend.surname == surname).all()
         response = json.dumps(clients_hired_friend, cls=AlchemyEncoder)
         return response
 
     @staticmethod
-    def get_rented_friends_by_client_time_rents_and_date(engine, client_id, start_date, finish_date,
-                                                   rents):
-        with engine.connect() as connection:
-            sql_statement = f"""select friend.name, friend.surname from client c
-                            inner join meeting m using(client_id)
-                            inner join friend_group using(friend_group_id) 
-                            inner join friend_group_record using(friend_group_id) 
-                            inner join friend using(friend_id)
-                            where c.client_id = {client_id} and m.date between {start_date} and {finish_date}
-                            group by friend.friend_id
-                            having count(friend.friend_id) >= {rents};"""
-
+    def get_clients_who_rented_by_date_rents_and_number(sql_engine, friend_id, start_date, end_date, rents):
+        with sql_engine.connect() as connection:
+            sql_statement = f"""select profile.name, profile.surname from friend f
+                                left join profile using (profile_id)
+                                inner join friend_group_record using(friend_id) 
+                                inner join friend_group using(friend_group_id) 
+                                inner join meeting m using(friend_group_id)
+                                inner join client c using(client_id)
+                                where f.friend_id = {friend_id} and m.date between {start_date} and {end_date}
+                                group by c.client_id
+                                having count(c.client_id) >= {rents};"""
             result = connection.execute(text(sql_statement))
         response = json.dumps(result, cls=AlchemyEncoder)
         return response
--- src/api/friend_queries.py
+++ src/api/friend_queries.py
@@ -18,6 +18,7 @@ from models.friend_group import FriendGroup
 from models.friend_group_record import FriendGroupRecord
 from models.holiday import Holiday
 from models.meeting import Meeting
+from models.profile import Profile
 from models.serializer import AlchemyEncoder
 
 
@@ -28,52 +29,75 @@ class FriendQueries(Resource):
         session = Session(bind=engine)
         if request_type == 'get_all':
             return FriendQueries.get_all_friends(session)
+
         elif request_type == 'get_available':
             return FriendQueries.get_available_friends(session)
+
         elif request_type == 'get_by_client_id':
             client_id = int(request.form['client_id'])
             return FriendQueries.get_by_client_id(session, client_id)
+
         elif request_type == 'get_by_name':
             name = request.form['name']
             surname = request.form['surname']
             return FriendQueries.get_by_name(session, name, surname)
+
         elif request_type == 'get_average_complained_clients_in_group_by_months':
             friend_id = request.form['friend_id']
             return FriendQueries.get_average_complained_clients_in_group_by_months(session, friend_id)
+
         elif request_type == 'get_days_when_was_number_of_friends_available':
             min_friends = request.form['min_friends_number']
             max_friends = request.form['max_friends_number']
             return FriendQueries.get_days_when_was_number_of_friends_available(session, min_friends, max_friends)
 
+        elif request_type == 'get_all_friends_by_rents_and_date':
+            start_date = request.form['start_date']
+            end_date = request.form['end_date']
+            rents = request.form['rents']
+            return FriendQueries.get_all_friends_by_rents_and_date(engine, rents, start_date, end_date)
+
+        elif request_type == 'get_rented_friends_by_client_time_rents_and_date':
+            client_id = request.form['client_id']
+            start_date = request.form['start_date']
+            end_date = request.form['end_date']
+            rents = request.form['rent']
+
+            return FriendQueries.get_rented_friends_by_client_time_rents_and_date(engine, client_id, start_date,
+                                                                                  end_date, rents)
+
     @staticmethod
     def get_all_friends(session):
-        friends_all = session.query(Friend).all()
+        friends_all = session.query(Friend, Profile).select_from(Friend).join(Profile).all()
         response = json.dumps(friends_all, cls=AlchemyEncoder)
         return response
 
     @staticmethod
     def get_available_friends(session):
         now = date.today()
-        before_available_friends = session.query(Friend, Holiday).select_from(Friend).join(Holiday).filter(
-            now < Holiday.start_date).all()
-        after_available_friends = session.query(Friend, Holiday).select_from(Friend).join(Holiday).filter(
-            now > Holiday.end_date).all()
+        before_available_friends = session.query(Friend, Holiday, Profile).select_from(Friend). \
+            join(Profile).join(Holiday).filter(now < Holiday.start_date).all()
+
+        after_available_friends = session.query(Friend, Holiday, Profile).select_from(Friend). \
+            join(Profile).join(Holiday).filter(now > Holiday.end_date).all()
 
         available_friends = before_available_friends + after_available_friends
-        available_friends = [friend.Friend for friend in available_friends]
+        available_friends = [[friend.Friend, friend.Profile] for friend in available_friends]
         response = json.dumps(available_friends, cls=AlchemyEncoder)
         return response
 
     @staticmethod
     def get_by_client_id(session, client_id):
-        friends_by_client_id = session.query(Friend).join(FriendGroupRecord).join(FriendGroup).join(Meeting).filter(
+        friends_by_client_id = session.query(Friend, Profile).select_from(Friend).join(Profile).join(
+            FriendGroupRecord).join(FriendGroup).join(Meeting).filter(
             Meeting.client_id == client_id).all()
         response = json.dumps(friends_by_client_id, cls=AlchemyEncoder)
         return response
 
     @staticmethod
     def get_by_name(session, name, surname):
-        friends_by_name = session.query(Friend).filter(Friend.surname == surname).filter(Friend.name == name).all()
+        friends_by_name = session.query(Friend, Profile).select_from(Friend).join(Profile).filter(
+            Profile.surname == surname).filter(Profile.name == name).all()
         response = json.dumps(friends_by_name, cls=AlchemyEncoder)
         return response
 
@@ -102,24 +126,10 @@ class FriendQueries(Resource):
         return response
 
     @staticmethod
-    def get_clients_who_rented_by_date_rents_and_number(engine, friend_id, start_date, end_date, rents):
-        with engine.connect() as connection:
-            sql_statement = f"""select c.name, c.surname from friend f
-                                inner join friend_group_record using(friend_id) 
-                                inner join friend_group using(friend_group_id) 
-                                inner join meeting m using(friend_group_id)
-                                inner join client c using(client_id)
-                                where f.friend_id = {friend_id} and m.date between {start_date} and {end_date}
-                                group by c.client_id
-                                having count(c.client_id) >= {rents};"""
-            result = connection.execute(text(sql_statement))
-        response = json.dumps(result, cls=AlchemyEncoder)
-        return response
-
-    @staticmethod
-    def get_all_friends_by_rents_and_date(engine, start_date, end_date, rents):
-        with engine.connect() as connection:
-            sql_statement = f"""select friend.name, friend.surname from client c 
+    def get_all_friends_by_rents_and_date(sql_engine, start_date, end_date, rents):
+        with sql_engine.connect() as connection:
+            sql_statement = f"""select profile.name, profile.surname from client c
+                                left join profile using (profile_id)
                                 inner join meeting m using(client_id)
                                 inner join friend_group using(friend_group_id) 
                                 inner join friend_group_record using(friend_group_id) 
@@ -127,6 +137,24 @@ class FriendQueries(Resource):
                                 where m.date between {start_date} and {end_date}
                                 group by friend.friend_id
                                 having count(friend.friend_id) >= {rents};"""
+            result = connection.execute(text(sql_statement))
+        response = json.dumps(result, cls=AlchemyEncoder)
+        return response
+
+    @staticmethod
+    def get_rented_friends_by_client_time_rents_and_date(sql_engine, client_id, start_date, end_date,
+                                                         rents):
+        with sql_engine.connect() as connection:
+            sql_statement = f"""select profile.name, profile.surname from client c
+                                left join profile using (profile_id)
+                                inner join meeting m using(client_id)
+                                inner join friend_group using(friend_group_id) 
+                                inner join friend_group_record using(friend_group_id) 
+                                inner join friend using(friend_id)
+                                where c.client_id = {client_id} and m.date between {start_date} and {end_date}
+                                group by friend.friend_id
+                                having count(friend.friend_id) >= {rents};"""
+
             result = connection.execute(text(sql_statement))
         response = json.dumps(result, cls=AlchemyEncoder)
         return response
