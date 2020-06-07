61995b83d1f867cf63544568b39fe1fb0573df0d
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-06-08 00:09:02 +0300

    New query implemented

--- src/api/holiday_queries.py
+++ src/api/holiday_queries.py
@@ -3,6 +3,7 @@ from datetime import date
 
 from flask import request
 from flask_restful import Resource
+from sqlalchemy import text
 from sqlalchemy.orm import Session
 
 from models import engine
@@ -22,3 +23,17 @@ class HolidayQueries(Resource):
     @staticmethod
     def get_number_holidays(session, num_friends):
         num_friends = session.query(Holiday).all()
+
+    @staticmethod
+    def get_day_when_friends_had_holidays(sql_engine, min_friends_absent, max_friends_absent, start_date, end_date):
+        with sql_engine.connect() as connection:
+            sql_statement = f"""select day::date, count(*) as friends_on_holiday
+                                from generate_series(date {start_date}, date {end_date}, '1 day') as gs(day)
+                                left join holiday h on h.start_date <= day and h.end_date >= day
+                                left join friend f on h.friend_id = f.friend_id
+                                group by day
+                                having count(*) > {min_friends_absent}
+                                and count(*) < {max_friends_absent};"""
+            result = connection.execute(text(sql_statement))
+        response = json.dumps(result, cls=AlchemyEncoder)
+        return response
