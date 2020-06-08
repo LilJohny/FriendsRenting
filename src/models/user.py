883012d77a223882ddff6fcb1f72a7609d86a82f
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-06-09 00:22:02 +0300

    Implemented User model

---
+++ src/models/user.py
@@ -0,0 +1,11 @@
+from flask_login import UserMixin
+
+from models import db
+from models.profile import Profile
+
+
+class User(UserMixin, db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    profile_id = db.Column(db.Integer, db.ForeignKey('profile.profile_id'), unique=True, nullable=False)
+    friend_id = db.Column(db.Integer, db.ForeignKey('friend.friend_id'), unique=True, nullable=True)
+    client_id = db.Column(db.Integer, db.ForeignKey('client.client_id'), unique=True, nullable=True)
