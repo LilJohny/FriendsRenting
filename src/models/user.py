91b9706e0fb9df361bc2f0e1c44706bb18d960a4
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-06-09 04:13:10 +0300

    User model

--- src/models/user.py
+++ src/models/user.py
@@ -9,3 +9,10 @@ class User(UserMixin, db.Model):
     profile_id = db.Column(db.Integer, db.ForeignKey('profile.profile_id'), unique=True, nullable=False)
     friend_id = db.Column(db.Integer, db.ForeignKey('friend.friend_id'), unique=True, nullable=True)
     client_id = db.Column(db.Integer, db.ForeignKey('client.client_id'), unique=True, nullable=True)
+
+    @staticmethod
+    def check_password(hashed_password, password):
+        return hashed_password == password
+
+    def get_id(self):
+        return self.user_id
